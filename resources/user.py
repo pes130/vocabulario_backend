e1d6e20d4e556440cd8982c28af25931ecf98fe0
pes130 <pablo.esteban.sanchez@gmail.com>
2019-07-13 21:40:01 +0200

    token freshness set to true

--- resources/user.py
+++ resources/user.py
@@ -64,7 +64,7 @@ class TokenRefresh(Resource):
     @jwt_refresh_token_required
     def post(self):
         current_user_id = get_jwt_identity()
-        new_token = create_access_token(identity=current_user_id, fresh=False)
+        new_token = create_access_token(identity=current_user_id, fresh=True)
         token_info = decode_token(new_token)
         expires = token_info['exp']
         return {
